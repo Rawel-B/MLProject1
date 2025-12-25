a6a5749295b05832c0ca4e46eb66d401e4876830
Rawel B <rawel.bayoudh@gmail.com>
2025-12-25 16:21:05 +0100

    cloud deployment update

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,10 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+#client = MongoClient("mongodb://localhost:27017/") # Local
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://rawelb:LCXi5y5NLUy67yau@wealthai.9kakug7.mongodb.net/")
+client = MongoClient(MONGO_URI)
+
 db = client.ml_project
 users = db.users
 reports = db.reports
